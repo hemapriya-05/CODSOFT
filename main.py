b26a579f6bb1c27eca35e2e5b1a7aa7cfcb9f5c3
hemapriya-05 <hemapriya0510@gmail.com>
2026-06-27 19:37:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+operation = input("choose an operation(+, -, *, /, **, //, %):")
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    result = num1 / num2
+elif operation == "**":
+    result = num1 ** num2
+elif operation == "//":
+    result = num1 // num2
+elif operation == "%":
+    result = num1 % num2
+else:
+    print("Invalid value")
+print("Result:", result)    
+
